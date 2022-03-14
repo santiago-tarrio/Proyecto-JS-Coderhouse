4ba1858794ea8f52d06bde9db7d12ab58bfcff0a
Santiago Tarrio <tarrio.sant@gmail.com>
2022-03-14 16:27:12 -0300

    arrays

--- src/main.js
+++ src/main.js
@@ -1,15 +1,27 @@
-function Producto(nombre, precio, stock) {
-    this.nombre = nombre;
-    this.precio = precio;
-    this.stock= stock;
+class Producto{
+    constructor (nombre, precio, stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock= stock;
+    }
 }
 
-const producto1 = new Producto("Jersey Los Angeles Lakers", 25000, 2);
-const producto2 = new Producto("Jersey Phoenix Suns", 25000, 5);
-const producto3 = new Producto("Jersey Denver Nuggets", 25000, 3);
-const producto4 = new Producto("Gorra Brooklyn Nets", 12000, 6);
-const producto5 = new Producto("Short Boston Celtics", 16000, 10);
-const producto6 = new Producto("Pantalon Los Angeles Clippers", 20000, 1);
+const productos = [];
+
+var productoArray = (new Producto("Jersey Los Angeles Lakers", 25000, 2));
+productos.push(productoArray)
+var productoArray = (new Producto("Jersey Phoenix Suns", 25000, 5));
+productos.push(productoArray)
+var productoArray = (new Producto("Jersey Denver Nuggets", 25000, 3));
+productos.push(productoArray)
+var productoArray = (new Producto("Gorra Brooklyn Nets", 12000, 6));
+productos.push(productoArray)
+var productoArray = (new Producto("Short Boston Celtics", 16000, 10));
+productos.push(productoArray)
+var productoArray = (new Producto("Pantalon Los Angeles Clippers", 20000, 1));
+productos.push(productoArray)
+
+const carrito = []
 
 let total=0;
 
@@ -25,33 +37,33 @@ let hayStock;
 do{
     const seleccionDeProducto = prompt (`
         Seleccione un producto para agregar al carrito:
-        1. ${producto1.nombre} a $ ${producto1.precio} 
-        2. ${producto2.nombre} a $ ${producto2.precio} 
-        3. ${producto3.nombre} a $ ${producto3.precio} 
-        4. ${producto4.nombre} a $ ${producto4.precio}
-        5. ${producto5.nombre} a $ ${producto5.precio}
-        6. ${producto6.nombre} a $ ${producto6.precio}`);
+        1. ${productos[0].nombre} a $ ${productos[0].precio} 
+        2. ${productos[1].nombre} a $ ${productos[1].precio} 
+        3. ${productos[2].nombre} a $ ${productos[2].precio} 
+        4. ${productos[3].nombre} a $ ${productos[3].precio}
+        5. ${productos[4].nombre} a $ ${productos[4].precio}
+        6. ${productos[5].nombre} a $ ${productos[5].precio}`);
     
     const seleccionDeStock = prompt ("Seleccione la cantidad que desea comprar de este producto:");
 
     switch (seleccionDeProducto) {
         case "1":
-            disponibilidadStock = producto1.stock
+            disponibilidadStock = productos[0].stock
             break;
         case "2":
-            disponibilidadStock = producto2.stock
+            disponibilidadStock = productos[1].stock
             break;
         case "3":
-            disponibilidadStock = producto3.stock
+            disponibilidadStock = productos[2].stock
             break;
         case "4":
-            disponibilidadStock = producto4.stock
+            disponibilidadStock = productos[3].stock
             break;
         case "5":
-            disponibilidadStock = producto5.stock
+            disponibilidadStock = productos[4].stock
             break;
         case "6":
-            disponibilidadStock = producto6.stock
+            disponibilidadStock = productos[5].stock
             break;
         default:
             alert ("NO ES VALIDO");
@@ -69,43 +81,49 @@ do{
         console.log (stockProducto);
         switch (stockProducto) {
             case "1s":
-                alert (`agregaste ${producto1.nombre}`)
-                total+=(producto1.precio*seleccionDeStock);
+                alert (`agregaste ${productos[0].nombre}`)
+                total+=(productos[0].precio*seleccionDeStock);
+                carrito.push(productos[0])
                 break;
             case "1n":
                 alert ("no hay stock")
                 break;
             case "2s":
-                alert (`agregaste ${producto2.nombre}`)
-                total+=producto2.precio*seleccionDeStock;
+                alert (`agregaste ${productos[1].nombre}`)
+                total+=productos[1].precio*seleccionDeStock;
+                carrito.push(productos[1])
                 break;
             case "2n":
                 alert ("no hay stock")
                 break;
             case "3s":
-                alert (`agregaste ${producto3.nombre}`)
-                total+=(producto3.precio*seleccionDeStock);
+                alert (`agregaste ${productos[2].nombre}`)
+                total+=(productos[2].precio*seleccionDeStock);
+                carrito.push(productos[2])
                 break;
             case "3n":
                 alert ("no hay stock")
                 break;
             case "4s":
-                alert (`agregaste ${producto4.nombre}`)
-                total+=producto4.precio*seleccionDeStock;
+                alert (`agregaste ${productos[3].nombre}`)
+                total+=productos[3].precio*seleccionDeStock;
+                carrito.push(productos[3])
                 break;
             case "4n":
                 alert ("no hay stock")
                 break;
             case "5s":
-                alert (`agregaste ${producto5.nombre}`)
-                total+=(producto5.precio*seleccionDeStock);
+                alert (`agregaste ${productos[4].nombre}`)
+                total+=(productos[4].precio*seleccionDeStock);
+                carrito.push(productos[4])
                 break;
             case "5n":
                 alert ("no hay stock")
                 break;
             case "6s":
-                alert (`agregaste ${producto6.nombre}`)
-                total+=producto6.precio*seleccionDeStock;
+                alert (`agregaste ${productos[5].nombre}`)
+                total+=productos[5].precio*seleccionDeStock;
+                carrito.push(productos[5])
                 break;
             case "6n":
                 alert ("no hay stock")
@@ -123,3 +141,5 @@ do{
         }else{
             alert ("el total es " + total + " o en 12 cuotas de " + descuentoConInteres)
         }
+
+console.log(carrito)
