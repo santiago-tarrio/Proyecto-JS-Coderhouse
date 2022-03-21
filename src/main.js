90745dd06d79ad0a08f2ce8618b7dcd75836b026
Santiago Tarrio <tarrio.sant@gmail.com>
2022-03-21 14:48:06 -0300

    dom

--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
 class Producto{
-    constructor (nombre, precio, stock) {
+    constructor (nombre, precio, stock, imagen) {
         this.nombre = nombre;
         this.precio = precio;
         this.stock= stock;
+        this.imagen = imagen; 
     }
 }
 
 const productos = [];
 
-var productoArray = (new Producto("Jersey Los Angeles Lakers", 25000, 2));
+var productoArray = (new Producto("Jersey Los Angeles Lakers", 25000, 2, "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/019acd9b-fd80-4ddd-902d-4b96e28a0671/jersey-nba-swingman-lebron-james-lakers-GTRSXs.png" ));
 productos.push(productoArray)
-var productoArray = (new Producto("Jersey Phoenix Suns", 25000, 5));
+var productoArray = (new Producto("Jersey Phoenix Suns", 25000, 5, "https://images.footballfanatics.com/phoenix-suns/phoenix-suns-nike-city-edition-swingman-jersey-deandre-ayton-youth_ss4_p-12052266+pv-1+u-om8nwncrzqzfs88oclzs+v-cc9c17bb9dad423ebc15d0589c013c29.jpg?_hv=1&w=600"));
 productos.push(productoArray)
-var productoArray = (new Producto("Jersey Denver Nuggets", 25000, 3));
+var productoArray = (new Producto("Jersey Denver Nuggets", 25000, 3, "https://images.footballfanatics.com/denver-nuggets/denver-nuggets-nike-icon-edition-swingman-jersey-navy-nikola-jokic-mens_ss4_p-11972902+pv-1+u-ccro0a8kd92fbgch8thg+v-c2e238a3f7b546a98b1ea6b7340bbeb2.jpg?_hv=1&w=900"));
 productos.push(productoArray)
-var productoArray = (new Producto("Gorra Brooklyn Nets", 12000, 6));
+var productoArray = (new Producto("Gorra Brooklyn Nets", 12000, 6, "https://images.footballfanatics.com/brooklyn-nets/brooklyn-nets-new-era-the-league-9forty-adjustable-cap_ss4_p-11896022+u-8wh4xk1s2wecqry69zs0+v-c47f1b8820f6417aa1174661542cc83f.jpg?_hv=1&w=600"));
 productos.push(productoArray)
-var productoArray = (new Producto("Short Boston Celtics", 16000, 10));
+var productoArray = (new Producto("Short Boston Celtics", 16000, 10, "https://images.footballfanatics.com/boston-celtics/boston-celtics-nike-icon-swingman-shorts-mens_ss4_p-11914513+u-yxdp39thg5asfcni9drm+v-bfea50cedef04222a2ff57cf9195235f.jpg?_hv=1&w=600"));
 productos.push(productoArray)
 var productoArray = (new Producto("Pantalon Los Angeles Clippers", 20000, 1));
 productos.push(productoArray)
@@ -29,7 +30,7 @@ let total=0;
 
 const indiceArray = productos.map(e => e.stock).indexOf(0);
 productos.splice(indiceArray, 1);
-console.log(productos)
+
 
 function aplicarDescuento(parametro){
     if(parametro<1300){
@@ -147,4 +148,32 @@ do{
             alert ("el total es " + total + " o en 12 cuotas de " + descuentoConInteres)
         }
 
-console.log(carrito)
+
+generarCards(productos);
+
+function generarCards(bloqueDeProductos){
+    let productosEnVenta = ``;
+    bloqueDeProductos.forEach((datosArray) => {
+        productosEnVenta += `<div class="producto">
+        <p>NBA</p>
+        <img src="${datosArray.imagen}" class="imagenProducto"  onError="eliminarImagen(this);">
+        <p>${datosArray.nombre}</p>
+        <p>${datosArray.precio}</p>
+    </div>`;
+    });
+    inclusionDeProductos(productosEnVenta);
+}
+
+
+function inclusionDeProductos(cards) {
+    document.getElementById("contenedorProductos").innerHTML = cards;
+};
+
+function eliminarImagen(element) {
+    element.parentNode.remove();
+}
+
+
+
+
+
